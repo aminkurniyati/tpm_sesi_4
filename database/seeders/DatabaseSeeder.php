1215d44d35b51ed0fec076cd1e1f612f0cf5663a
aminkurniyati <amin.kurniyati@gmail.com>
2024-04-05 02:16:53 +0700

    Make laravel/ui and UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(SiswaSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::get('template', function () {
 });
 
 Route::resource('siswa', SiswaController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
